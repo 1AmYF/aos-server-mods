d3a60cba0c6d3b9d588221bd909f8d9e421e3a45
1AmYF <1AmYF@users.noreply.github.com>
2020-01-02 15:10:21 +0100

    Add build, challenge, griefwatch, parkour, votenext

---
+++ scripts/pyspades/griefwatch.py
@@ -0,0 +1,249 @@
+"""
+griefwatch.py by IAmYourFriend https://twitter.com/1AmYF
+
+griefwatch.py reports warnings on suspected grief activity. The warning will
+be sent to IRC, written into the log file and also sent ingame to players
+with a staff role. Two commands allow tracking of block activity.
+
+The warnings depend on the gamemode:
+    - CTF or any other normal gamemode: reports removal of team blocks
+    - Build: Reports removal of both team and map blocks
+    - Push: reports block spam at spawn, pillar spam (= blocking path),
+      breaking blocks while on enemy side (= maybe teleport/other glitch)
+
+Block removal warnings are triggered when removing lots of blocks in a short
+time. Note that a warning alone is no proof that the player was really
+griefing.
+
+Setup:
+
+    It is important to put this script *BEFORE* blockinfo in the config script
+    list. It will not work otherwise.
+
+Commands:
+
+    /inspect
+        Show coordinates of a block, it's color values and who placed it.
+    /blocklog
+        Every block removal will be logged into a file (intended only for
+        temporary use). Find it as block_removal.log in the log folder.
+        Columns in the log file:
+        name, id, team, tool, weapon, position, blockposition, distance, ip, time
+"""
+
+from pyspades.constants import *
+from commands import add, admin
+from pyspades.collision import distance_3d_vector
+from pyspades.common import prettify_timespan, to_coordinates
+from twisted.internet.reactor import seconds
+
+DEFAULTMODE, BUILD, PUSH = xrange(3)
+
+BLOCK_REMOVAL_MAX = 20
+BLOCK_REMOVAL_MINS = 2
+BLOCK_TEAM_REMOVAL_MAX = 20
+BLOCK_TEAM_REMOVAL_MINS = 2
+SPAWN_BLOCKS_MAX = 200
+PILLAR_HEIGHT_MAX = 4
+PILLARS_MAX = 10
+
+
+def get_now_in_secs():
+    return int(time.time())
+
+
+@admin
+def inspect(connection):
+    if not connection.block_inspect:
+        connection.block_inspect = True
+        return "Hit a block to inspect it."
+    else:
+        connection.block_inspect = False
+        return "No longer inspecting blocks."
+
+
+@admin
+def blocklog(connection):
+    if not connection.protocol.block_log:
+        connection.protocol.block_log = True
+        return "Block removal logging enabled."
+    else:
+        connection.protocol.block_log = False
+        return "Block removal logging disabled."
+
+
+add(inspect)
+add(blocklog)
+
+
+def log_block_removal(self, x, y, z):
+    f = open("logs/block_removal.log", "a")
+    self.blockposition.x = x
+    self.blockposition.y = y
+    self.blockposition.z = z
+    distance = int(distance_3d_vector(self.world_object.position, self.blockposition))
+    sep = ";"
+    info = (self.name + sep + str(self.player_id) + sep + str(self.team.id) + sep +
+            str(self.tool) + sep + str(self.weapon) + sep +
+            str((int(self.world_object.position.x),
+                 int(self.world_object.position.y),
+                 int(self.world_object.position.z))) +
+            sep + str((x, y, z)) + sep + str(distance) + sep +
+            self.address[0] + sep + str(seconds()))
+    f.write(info + "\n")
+    f.close()
+
+
+def is_in_area(x, y, top_x, top_y, bottom_x, bottom_y):
+    return top_x <= x < bottom_x and top_y <= y < bottom_y
+
+
+def send_warning(protocol, message):
+    print message
+    for player in protocol.players.values():
+        if player.admin or player.user_types.moderator or player.user_types.guard:
+            player.send_chat(message)
+    irc_relay = protocol.irc_relay
+    if irc_relay:
+        if irc_relay.factory.bot and irc_relay.factory.bot.colors:
+            message = "\x0304" + message + "\x0f"
+        irc_relay.send(message)
+
+
+def apply_script(protocol, connection, config):
+    class GriefWatchConnection(connection):
+        class blockposition():
+            x = 0
+            y = 0
+            z = 0
+        block_inspect = False
+        spawn_blocks = 0
+        pillar_blocks = 0
+        pillar_count = 0
+        pillar_counted = False
+        pillar_last_xy = None
+        located_on_enemy_side = False
+        last_block_time = 0
+
+        def check_for_block_removal(self, x, y, z, team_blocks_only=False):
+            if self.blocks_removed is not None:
+                time = seconds() - (BLOCK_TEAM_REMOVAL_MINS if team_blocks_only
+                                    else BLOCK_REMOVAL_MINS) * 60.0
+                block_time = 0
+                amount = 0
+                for b in reversed(self.blocks_removed):
+                    if (team_blocks_only and b[1] is not None and b[1][0] != self.name and
+                        b[1][1] == self.team.id) or (not team_blocks_only and
+                                                     (b[1] is None or b[1][0] != self.name)):
+                        if block_time == 0:
+                            block_time = b[0]
+                        if b[0] < time:
+                            break
+                        amount += 1
+                if (amount > 1 and block_time > self.last_block_time + 30 and
+                        not amount % (BLOCK_TEAM_REMOVAL_MAX if team_blocks_only
+                                      else BLOCK_REMOVAL_MAX)):
+                    send_warning(self.protocol, "Warning: %s #%s removed %s%s blocks (%s)"
+                                 % (self.name, self.player_id, amount,
+                                    " team" if team_blocks_only else "",
+                                    to_coordinates(x, y)))
+                    self.last_block_time = block_time
+
+        def check_for_spawn_block_spam(self, x, y):
+            range = 10  # spawn_range
+            area = self.team.spawn
+            if is_in_area(x, y, area[0] - range, area[1] - range,
+                          area[0] + range, area[1] + range):
+                self.spawn_blocks += 1
+            if self.spawn_blocks >= SPAWN_BLOCKS_MAX:
+                send_warning(self.protocol, "Warning: Potential block spam at spawn by %s #%s"
+                             % (self.name, self.player_id))
+                self.spawn_blocks = 0
+
+        def check_for_pillar_block_spam(self, x, y):
+            if self.pillar_last_xy is not None:
+                if self.pillar_last_xy[0] == x and self.pillar_last_xy[1] == y:
+                    if not self.pillar_counted:
+                        self.pillar_blocks += 1
+                else:
+                    self.pillar_blocks = 0
+                    self.pillar_counted = False
+                if self.pillar_blocks >= PILLAR_HEIGHT_MAX - 1:
+                    self.pillar_count += 1
+                    self.pillar_counted = True
+                    self.pillar_blocks = 0
+                if self.pillar_count >= PILLARS_MAX:
+                    send_warning(self.protocol, "Warning: Potential pillar spam by %s #%s"
+                                 % (self.name, self.player_id))
+                    self.pillar_count = 0
+            self.pillar_last_xy = (x, y)
+
+        def check_for_located_on_enemy_side(self):
+            area = self.team.other.build_area
+            if (area is not None and self.world_object is not None and
+                    is_in_area(self.world_object.position.x,
+                               self.world_object.position.y,
+                               *area)):
+                if not self.located_on_enemy_side:
+                    x, y, z = self.get_location()
+                    send_warning(self.protocol, "Warning: %s #%s is on enemy side (%s)"
+                                 % (self.name, self.player_id, to_coordinates(x, y)))
+                self.located_on_enemy_side = True
+
+        def on_spawn(self, pos):
+            self.located_on_enemy_side = False
+            return connection.on_spawn(self, pos)
+
+        def on_block_build(self, x, y, z):
+            if self.protocol.current_mode == PUSH:
+                self.check_for_spawn_block_spam(x, y)
+                self.check_for_pillar_block_spam(x, y)
+            return connection.on_block_build(self, x, y, z)
+
+        def on_line_build(self, points):
+            if self.protocol.current_mode == PUSH:
+                for point in points:
+                    self.check_for_spawn_block_spam(point[0], point[1])
+            return connection.on_line_build(self, points)
+
+        def on_block_destroy(self, x, y, z, mode):
+            if self.block_inspect:
+                message = ("Position " + str((x, y, z)) + ", Color " +
+                           str(self.protocol.map.get_color(x, y, z)))
+                info = None
+                if self.protocol.block_info is not None:
+                    if (x, y, z) in self.protocol.block_info:
+                        info = self.protocol.block_info[(x, y, z)]
+                    else:
+                        info = None
+                if info is not None:
+                    message += ", placed by %s (%s)" % (info[0], "Green" if info[1] else "Blue")
+                else:
+                    message += ", map block"
+                self.send_chat(message)
+                return False
+            return connection.on_block_destroy(self, x, y, z, mode)
+
+        def on_block_removed(self, x, y, z):
+            if self.protocol.current_mode == PUSH and not self.located_on_enemy_side:
+                self.check_for_located_on_enemy_side()
+            elif self.protocol.current_mode == BUILD:
+                self.check_for_block_removal(x, y, z)
+            elif self.protocol.current_mode == DEFAULTMODE:
+                self.check_for_block_removal(x, y, z, team_blocks_only=True)
+            if self.protocol.block_log:
+                log_block_removal(self, x, y, z)
+            connection.on_block_removed(self, x, y, z)
+
+    class GriefWatchProtocol(protocol):
+        current_mode = DEFAULTMODE
+        block_log = False
+
+        def on_map_change(self, map):
+            if self.game_mode_name.lower() == "build":
+                self.current_mode = BUILD
+            elif self.game_mode_name.lower() == "push":
+                self.current_mode = PUSH
+            return protocol.on_map_change(self, map)
+
+    return GriefWatchProtocol, GriefWatchConnection
---
+++ scripts/pyspades/parkour.py
@@ -0,0 +1,229 @@
+"""
+parkour.py by IAmYourFriend https://twitter.com/1AmYF
+
+parkour.py is a parkour gamemode with highscores. Players spawn on the same
+team at the same location and have to make it to their base to complete the
+parkour. Example: https://youtu.be/CIvmWNBpfi8
+
+Setup:
+
+    Set game_mode in your server config to "parkour" and add parkour maps to the
+    rotation.
+
+    To create a new parkour map, map txt metadata is required. Example:
+
+        extensions = {
+            'water_damage' : 100,
+            'parkour_start' : (127, 256, 50),
+            'parkour_end' : (382, 256, 50),
+            'parkour_checkpoints' : ((187, 256, 50), (240, 256, 39), (289, 256, 50))
+        }
+
+    'parkour_start' marks the coordinate for spawn, 'parkour_end' for the base
+    location. If a player dies during the parkour, he will spawn to the closest
+    checkpoint coordinate behind him (the parkour direction needs to be from
+    left to right on the map view).
+
+Commands:
+
+    /highscore
+        List the top highscores (if enabled).
+    /reset
+        Reset your time/score and retry the parkour from start.
+"""
+
+import time
+import operator
+from pyspades.constants import *
+from commands import add
+from math import floor
+import os.path
+
+# Every parkour completion will be saved into a csv file and the top scores will be
+# listed with the /highscore command (the csv file will be written into the map
+# folder as mapname_scores.csv).
+SAVE_HIGHSCORES = True
+
+# How many of the top scores to show when using the /highscore command.
+SHOW_SCORES = 10
+
+CSV_SEP = ";"
+HIDE_COORD = (0, 0, 63)
+
+
+def get_highscore_filename(connection):
+    return "maps/" + connection.protocol.map_info.rot_info.name + "_scores.csv"
+
+
+def highscore(connection):
+    if not os.path.exists(get_highscore_filename(connection)):
+        return "No highscores yet"
+    scores = list()
+    file = open(get_highscore_filename(connection), "r")
+    for line in file:
+        if line is not None and len(line.strip()) > 0:
+            scores.append(line.strip().split(CSV_SEP))
+
+    if len(scores) > 0:
+        sortedscores = sorted(scores, key=operator.itemgetter(1))
+        displayscores = list()
+        i = 1
+        for playervalues in sortedscores:
+            duplicate = False
+            for addedplayer in displayscores:
+                if compare_str_ignore_case(playervalues[0], addedplayer[0]):
+                    duplicate = True
+                    break
+            if not duplicate:
+                displayscores.append(playervalues)
+                i += 1
+                if i > SHOW_SCORES:
+                    break
+
+        i = 1
+        strscores = []
+        for displayvalues in displayscores:
+            place = str(i) + ". "
+            if i < 10:
+                place += " "
+            strscores.append(place + displayvalues[0] + "  (" + displayvalues[1] +
+                             " mins, deaths: " + displayvalues[2] + ")")
+            i += 1
+        connection.send_lines(strscores)
+
+
+def compare_str_ignore_case(str1, str2):
+    if str1 is not None and len(str1) > 0 and str2 is not None and len(str2) > 0:
+        return str1.lower().strip() == str2.lower().strip()
+    else:
+        return str1 == str2
+
+
+def reset(connection):
+    if connection.team is connection.protocol.blue_team:
+        connection.isresetting = True
+        connection.kill()
+
+if SAVE_HIGHSCORES:
+    add(highscore)
+add(reset)
+
+
+def save_highscore(connection, playername, parkourtime, deaths, playerip):
+    formatnow = time.strftime("%d.%m.%Y %H:%M:%S")
+    playernamesecure = ""
+    if playername is not None and len(playername) > 0:
+        playernamesecure = playername.replace(CSV_SEP, ",")
+    f = open(get_highscore_filename(connection), "a")
+    f.write("\n" + playernamesecure + CSV_SEP + str(parkourtime) + CSV_SEP +
+            str(deaths) + CSV_SEP + formatnow + CSV_SEP + str(playerip))
+    f.close()
+
+
+def get_now_in_secs():
+    return int(time.time())
+
+
+def get_formatted_parkour_time(completedseconds):
+    completedformatmin = str(int(floor(completedseconds / 60)))
+    if len(completedformatmin) == 1:
+        completedformatmin = "0" + completedformatmin
+    completedformatsec = str(int(completedseconds % 60))
+    if len(completedformatsec) == 1:
+        completedformatsec = "0" + completedformatsec
+    return completedformatmin + ":" + completedformatsec
+
+
+def reset_player_stats(self):
+    self.joinedtimestamp = get_now_in_secs()
+    self.completedparkour = False
+    self.reachedcheckpoint = 0
+    self.deathcount = 0
+
+
+def apply_script(protocol, connection, config):
+    class ParkourConnection(connection):
+        joinedtimestamp = None
+        completedparkour = False
+        reachedcheckpoint = 0
+        deathcount = 0
+        isresetting = False
+
+        def on_team_join(self, team):
+            if team is self.protocol.blue_team:
+                reset_player_stats(self)
+            return connection.on_team_join(self, team)
+
+        def on_flag_take(self):
+            return False
+
+        def on_spawn_location(self, pos):
+            if self.team is self.protocol.blue_team:
+                if self.isresetting:
+                    reset_player_stats(self)
+                self.isresetting = False
+                ext = self.protocol.map_info.extensions
+                if self.reachedcheckpoint > 0:
+                    return ext["parkour_checkpoints"][self.reachedcheckpoint - 1]
+                else:
+                    return ext["parkour_start"]
+            return connection.on_spawn_location(self, pos)
+
+        def on_kill(self, killer, type, grenade):
+            if self.team is self.protocol.blue_team and not self.isresetting:
+                self.deathcount += 1
+                checkpoints = self.protocol.map_info.extensions["parkour_checkpoints"]
+                i = len(checkpoints)
+                self.reachedcheckpoint = 0
+                for cp in reversed(checkpoints):
+                    if self.world_object.position.x >= cp[0]:
+                        self.reachedcheckpoint = i
+                        break
+                    i -= 1
+            return connection.on_kill(self, killer, type, grenade)
+
+        def on_refill(self):
+            if self.team is self.protocol.blue_team and not self.completedparkour:
+                self.completedparkour = True
+                if self.joinedtimestamp is not None:
+                    displaytime = get_formatted_parkour_time(get_now_in_secs() -
+                                                             self.joinedtimestamp)
+                    msg = "Congratulations, %s completed the parkour! Stats: %s mins, %s deaths"
+                    completedmessage = msg % (self.name, displaytime, self.deathcount)
+                    self.protocol.send_chat(completedmessage)
+                    self.protocol.irc_say(completedmessage)
+                    if SAVE_HIGHSCORES:
+                        save_highscore(self, self.name, displaytime,
+                                       self.deathcount, self.address[0])
+            return connection.on_refill(self)
+
+        def on_connect(self):
+            self.protocol.green_team.locked = True
+            self.protocol.balanced_teams = 0
+            self.protocol.building = False
+            self.protocol.fall_damage = False
+            return connection.on_connect(self)
+
+        def on_disconnect(self):
+            if self.team is self.protocol.blue_team and not self.completedparkour:
+                if self.joinedtimestamp is not None:
+                    displaytime = get_formatted_parkour_time(get_now_in_secs() -
+                                                             self.joinedtimestamp)
+                    msg = "%s ragequit after %s mins, %s deaths"
+                    failmessage = msg % (self.name, displaytime, self.deathcount)
+                    self.protocol.send_chat(failmessage)
+                    self.protocol.irc_say(failmessage)
+            connection.on_disconnect(self)
+
+    class ParkourProtocol(protocol):
+        game_mode = CTF_MODE
+
+        def on_base_spawn(self, x, y, z, base, entity_id):
+            if entity_id == BLUE_BASE:
+                return self.map_info.extensions["parkour_end"]
+            return HIDE_COORD
+
+        def on_flag_spawn(self, x, y, z, flag, entity_id):
+            return HIDE_COORD
+
+    return ParkourProtocol, ParkourConnection
